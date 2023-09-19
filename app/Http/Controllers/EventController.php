537d2f257fb3a41ffef892261b662de6e7917b3b
Diego Reis <diegoreso@me.com>
2023-09-19 14:03:32 -0300

    create events views

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -26,15 +26,20 @@ class EventController extends Controller
         $events->private = $request->private;
         $events->description = $request->description;
         
-        if($request->hasFile('image') || $request->file('image')->isValid()){
+        
+        if($request->hasFile('image') && $request->image->isValid()){
+
 
             $extension = $request->image->extension();
 
-            $imageName = md5($request->image->getClientOriginalName() .strtotime('now') ). "." . $extension;
+            $imageName = md5($request->image->getClientOriginalName() . strtotime('now')) . "." . $extension;
 
-            $request->image->move(public_path('/events/img'), $imageName);
-
+            $request->image->move(public_path('/img/events'), $imageName);
+            
             $events->image = $imageName;
+
+        
+
         }
 
         $events->save();
@@ -43,7 +48,13 @@ class EventController extends Controller
 
     }
 
+    public function show($id){
+        
+        $event = Event::FindOrFail($id);
 
+        return view('events.show', ['event' => $event]);
+
+    }
 
     public function create() {
         return view('events.create');
