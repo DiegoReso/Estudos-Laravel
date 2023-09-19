9c94ab138d0ea337602a7800b696f5b7bc5a15a5
Diego Reis <diegoreso@me.com>
2023-09-19 10:48:04 -0300

    create post events

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -17,6 +17,23 @@ class EventController extends Controller
 
     }
 
+
+    public function store(Request $request){
+
+        $events = new Event;
+        $events->title = $request->title;
+        $events->city = $request->city;
+        $events->private = $request->private;
+        $events->description = $request->description;
+        
+        $events->save();
+
+        return redirect('/')->with('msg', 'Evento criado com sucesso');
+
+    }
+
+
+
     public function create() {
         return view('events.create');
     }
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,4 @@ use App\Http\Controllers\EventController;
 Route::get('/', [EventController::class, 'index']);
 Route::get('/events/create', [EventController::class, 'create']);
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::post('/events', [EventController::class, 'store']);
